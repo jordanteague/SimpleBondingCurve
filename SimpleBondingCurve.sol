03ea1675b8a1c74e1a32acf890c2b3e78bfb6776
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-12-29 20:57:12 -0500

    Update SimpleBondingCurve.sol
    muh ocd

--- SimpleBondingCurve.sol
+++ SimpleBondingCurve.sol
@@ -5,70 +5,84 @@ pragma solidity >=0.8.0;
 import "./ERC20.sol";
 
 contract SimpleBondingCurve is ERC20 {
-
     uint256 public startingPrice; // wei
+
     uint256 public blockSize; // wei
+
     uint8 public blockPriceIncrement;
 
-    constructor() ERC20("MyDAO", "TEST", 18) {
+    constructor() ERC20('MyDAO', 'TEST', 18) {
         startingPrice = 10; // hardcoded values for ease of test deployment
+
         blockSize = 10;
+
         blockPriceIncrement = 10;
     }
 
-    function buy(uint256 amount_) external payable {
-        uint estPrice = this.estimatePrice(amount_);
-        require(msg.value >= estPrice, "INSUFFICIENT_FUNDS");
-        (bool sent, bytes memory data) = payable(address(this)).call{value: estPrice}("");
-        require(sent, "Failed to send Ether");
+    function buy(uint256 amount_) public payable {
+        uint256 estPrice = estimatePrice(amount_);
+
+        require(msg.value >= estPrice, 'INSUFFICIENT_FUNDS');
+
         _mint(msg.sender, amount_);
     }
 
     // separate external functions for testing purposes only
-    function getCurrentBlock() external view returns(uint) {
+    function getCurrentBlock() public view returns (uint256) {
         return (totalSupply / blockSize);
     }
 
-    function getRemainingInBlock() external view returns(uint) {
-        uint used = totalSupply % blockSize;
-        uint remaining = blockSize - used;
+    function getRemainingInBlock() public view returns (uint256) {
+        uint256 used = totalSupply % blockSize;
+
+        uint256 remaining = blockSize - used;
+
         return remaining;
     }
 
-    function getCurrentPrice() external view returns(uint) {
-        uint currentPrice = startingPrice;
-        uint currentBlock = this.getCurrentBlock();
-        for(uint i=0; i < currentBlock + 1; i++) {
-            currentPrice += (blockPriceIncrement * i);
+    function getCurrentPrice() public view returns (uint256) {
+        uint256 currentPrice = startingPrice;
+
+        uint256 currentBlock = getCurrentBlock();
+
+        for (uint256 i = 0; i < currentBlock++; i++) {
+            currentPrice += blockPriceIncrement * i;
         }
+
         return currentPrice;
     }
 
-    function estimatePrice(uint256 amount_) external view returns(uint) {
-        uint remainingInBlock = this.getRemainingInBlock();
-        uint currentPrice = this.getCurrentPrice();
-        uint estTotal;
-        if(amount_ <= remainingInBlock) {
+    function estimatePrice(uint256 amount_) public view returns (uint256) {
+        uint256 remainingInBlock = getRemainingInBlock();
+
+        uint256 currentPrice = getCurrentPrice();
+
+        uint256 estTotal;
+
+        if (amount_ <= remainingInBlock) {
             estTotal = amount_ * currentPrice;
         } else {
             estTotal += remainingInBlock * currentPrice;
+
             currentPrice += blockPriceIncrement;
-            uint remainingAmount = amount_ - remainingInBlock;
-            uint remainder = remainingAmount % blockSize;
-            uint blocksRemaining = remainingAmount / blockSize;
-            for(uint i=0; i < blocksRemaining; i++) {
+
+            uint256 remainingAmount = amount_ - remainingInBlock;
+
+            uint256 remainder = remainingAmount % blockSize;
+
+            uint256 blocksRemaining = remainingAmount / blockSize;
+
+            for (uint256 i = 0; i < blocksRemaining; i++) {
                 estTotal += currentPrice * blockSize;
-                currentPrice += (blockPriceIncrement * (i+1));
+
+                currentPrice += blockPriceIncrement * i++;
             }
-            if(remainder > 0) {
+
+            if (remainder != 0) {
                 estTotal += remainder * currentPrice;
             }
         }
+
         return estTotal;
     }
-    
-    receive() external payable {}
-
-    fallback() external payable {}
-
 }
